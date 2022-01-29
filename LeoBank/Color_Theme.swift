11d030229c475a6c12ae72918a78bcebeefa2acd
murads <murads01@icloud.com>
2022-01-30 01:22:37 +0400

    First

---
+++ LeoBank/Color_Theme.swift
@@ -0,0 +1,20 @@
+//
+//  Color_Theme.swift
+//  LeoBank
+//
+//  Created by Murad  on 29.01.22.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle {
+    static var yelowishGreen: Color {
+        Color(red: 238/255, green: 252/255, blue: 121/255)
+    }
+    
+    static var darkestGray: Color {
+        Color(red: 67/255, green: 61/255, blue: 72/255)
+    }
+}
+
--- LeoBank/StartPageView.swift
+++ LeoBank/StartPageView.swift
@@ -9,8 +9,58 @@ import SwiftUI
 
 struct StartPageView: View {
     var body: some View {
-        VStack {
-            
+        ZStack {
+            Color(red: 30/255, green: 33/255, blue: 33/255)
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Text("Leo")
+                        .font(.headline.bold())
+                        .foregroundColor(.yelowishGreen)
+                    + Text("Bank")
+                        .font(.headline)
+                        
+                }
+                Spacer()
+                HStack {
+                    Text("Leo")
+                        .font(.title.bold())
+                        .foregroundColor(.yelowishGreen)
+                    + Text("Bank")
+                        .font(.title)
+                        
+                }
+                Spacer()
+                VStack {
+                    NavigationLink {
+                        Text("Login Page")
+                    } label: {
+                        ZStack {
+                            LinearGradient(colors: [.white, .yelowishGreen],startPoint: .top, endPoint: .bottom)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            Text("Log in")
+                                .foregroundColor(.black)
+                        }
+                        .frame(height: 50)
+                        .padding(.horizontal)
+                    }
+                    Button {
+                        //login action
+                    } label: {
+                        ZStack {
+                             Color(red: 54/255, green: 51/255, blue: 57/255)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            Text("Become a client of the bank")
+                                .foregroundColor(.white)
+                        }
+                        .frame(height: 50)
+                        .padding(.horizontal)
+                    }
+                }
+                
+                Spacer()
+                Spacer()
+            }
         }
     }
 }
