b9277b7f1ce2709065a3f7a602d3bfdbfd0df62b
murads <murads01@icloud.com>
2022-01-29 22:12:50 +0400

    Initial Commit

---
+++ LeoBank/LeoBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeoBankApp.swift
+//  LeoBank
+//
+//  Created by Murad  on 29.01.22.
+//
+
+import SwiftUI
+
+@main
+struct LeoBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeoBank/StartPageView.swift
@@ -0,0 +1,22 @@
+//
+//  StartPageView.swift
+//  LeoBank
+//
+//  Created by Murad  on 29.01.22.
+//
+
+import SwiftUI
+
+struct StartPageView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct StartPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartPageView()
+    }
+}
